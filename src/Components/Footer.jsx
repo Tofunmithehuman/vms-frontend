26c40be01a0ce836481fe16dc91aa07048f0a329
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2024-07-18 14:40:43 +0100

    environment variable config

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import "../Styles/globalStyle.css";
 function Footer() {
   return (
     <div className="Footer">
-      <div className="bg-darkGray p-3 md:p-10 mt-20 flex justify-between items-center text-[10px] sm:text-sm">
+      <div className="bg-darkGray p-3 md:p-10 mt-20 flex justify-between items-center text-[8px] sm:text-sm">
         <h1 className="text-gray text-left">
           Kilometer 20 warri patani road, <br /> Ughelli, Delta State, Nigeria.{" "}
           <br /> Phone: 08145652652, 09062303605
