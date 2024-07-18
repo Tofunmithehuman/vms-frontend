517a7e91129630e85c316f33e98fd7a6c890dd5e
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2024-07-18 14:15:33 +0100

    update made

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,14 +3,15 @@ import "../Styles/globalStyle.css";
 function Footer() {
   return (
     <div className="Footer">
-      <div className="bg-darkGray p-4 md:p-10 mt-20 text-center md:text-left md:flex md:justify-between items-center">
-        <h1 className="text-gray text-sm">
+      <div className="bg-darkGray p-3 md:p-10 mt-20 flex justify-between items-center text-[7px] sm:text-sm">
+        <h1 className="text-gray text-left">
           Kilometer 20 warri patani road, <br /> Ughelli, Delta State, Nigeria.{" "}
           <br /> Phone: 08145652652, 09062303605
         </h1>
-        <h1 className=" text-gray text-sm">
+        <h1 className=" text-gray text-right">
           Email: info@transcorppower.com <br />© {new Date().getFullYear()}{" "}
-          Transcorp Power. All rights reserved.
+          Transcorp Power. All rights reserved. <br />
+          Developed by Tofunmithehuman.
         </h1>
       </div>
     </div>
--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+import env from "react-dotenv";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3500",
+  baseURL: env.BACKEND_URL || "http://localhost:3500",
 });
 
 export default instance;
